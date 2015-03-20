3ec563f2ae10954b7689f04f41e1007aa24389d9
WangYue <yuewang8806@hotmail.com>
2015-03-20 16:57:42 +0800

    test commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by yue wang on 15-3-20.
+//  Copyright (c) 2015年 cmcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by yue wang on 15-3-20.
+//  Copyright (c) 2015年 cmcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
